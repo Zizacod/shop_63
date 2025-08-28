ebad119a8516617d2a2093d30b0c49b1655c1969
Aziz Seidametov <pathfinder.r51offroad@gmail.com>
2025-08-28 18:13:16 +0200

    Initial commit

---
+++ src/main/java/ait/cohort63/shop_63/Shop63Application.java
@@ -0,0 +1,13 @@
+package ait.cohort63.shop_63;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop63Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop63Application.class, args);
+    }
+
+}
---
+++ src/test/java/ait/cohort63/shop_63/Shop63ApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.cohort63.shop_63;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop63ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
